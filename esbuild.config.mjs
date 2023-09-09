c09d7a3007fa83888f3eda7a71d2336a317577ef
Patrick Tournet <ptournet@users.noreply.github.com>
2023-09-09 20:43:05 +0200

    Test correcting Alpine errors

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -33,7 +33,7 @@ const context = await esbuild.context({
 		"@lezer/lr",
 		...builtins],
 	format: "cjs",
-	target: "es2016",
+	target: "es2018",
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
--- src/ui/rendering.ts
+++ src/ui/rendering.ts
@@ -1,13 +1,13 @@
 import { MarkdownPostProcessorContext } from "obsidian";
 import { Tabs } from "tabs";
 
-export function render(tabs: Tabs, source: string, container: HTMLElement, ctx: MarkdownPostProcessorContext): void {
+export function render(tabs: Tabs, source: string, container: HTMLElement, _: MarkdownPostProcessorContext): void {
 	if (!tabs.hasTabs()) {
 		renderCodeBlock(container, source, "html");
 		return;
 	}
 
-	const mainAttributes = { 'obs-x-data': "() => { tab: " + tabs.active_id + " }" };
+	const mainAttributes = { 'obs-x-data': "{ tab: " + tabs.active_id + " }" };
 	const divMain = container.createEl("div", {attr: mainAttributes});
 	const divTabs = divMain.createEl("div", { cls: ["html-tabs"] });
 	for (let index = 0; index < tabs.tabs.length; index++) {
